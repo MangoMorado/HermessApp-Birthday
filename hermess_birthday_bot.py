911dd1ddf2de371aeebaa94c359623ba27493eaf
Sergio Veloza <velozasergio@gmail.com>
2025-10-06 22:50:19 -0500

    Update hermess_birthday_bot.py

--- hermess_birthday_bot.py
+++ hermess_birthday_bot.py
@@ -41,6 +41,7 @@ class HermessBirthdayBot:
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+        chrome_options.add_argument("--user-data-dir=/tmp/selenium")
         
         # Comentar la siguiente línea si quieres ver el navegador en acción
         # chrome_options.add_argument("--headless")
